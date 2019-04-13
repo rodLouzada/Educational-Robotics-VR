a980a5e5926c69e2665a6b3b95613f07a1f0a3b7
rodLouzada <rodrigolouzada1@gmail.com>
2019-04-13 14:25:29 -0400

    First Commit
    Created the project folder
    Created the Testing room
    Created motor blocks
    Created Base block
    Created Wheels blocks
    Created Sonic Sensor Block
    Proggrammed "Snap in place" to snap cubes in place
    Code for VR pick up object

---
+++ Vr_Robotics/Assets/PickupObject.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    public Transform holder;
+    public Rigidbody rb;
+    private Vector3 positionHolder;
+    private Quaternion rotationHolder;
+    private bool saveGravity = false;
+    private float saveMaxAngularVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (holder != null)
+        {
+            Vector3 desiredPos = holder.localToWorldMatrix.MultiplyPoint(positionHolder);
+            Vector3 currentPos = this.transform.position;
+            Quaternion desiredRotation = holder.rotation * rotationHolder;
+            Quaternion currentRotation = this.transform.rotation;
+            rb.velocity = (desiredPos - currentPos) / Time.fixedDeltaTime;
+
+            Quaternion offsetRotation = desiredRotation * Quaternion.Inverse(currentRotation);
+            float angle; Vector3 axis;
+            offsetRotation.ToAngleAxis(out angle, out axis);
+            Vector3 rotationDiff = angle * Mathf.Deg2Rad * axis;
+            rb.angularVelocity = rotationDiff / Time.fixedDeltaTime;
+        }
+    }
+
+    public void pickedUp(Transform t)
+    {
+
+        if (holder != null)
+        {
+            return;
+        }
+        positionHolder = t.worldToLocalMatrix.MultiplyPoint(this.transform.position);
+        rotationHolder = Quaternion.Inverse(t.rotation) * this.transform.rotation;
+        saveGravity = rb.useGravity;
+        rb.useGravity = false;
+        saveMaxAngularVelocity = rb.maxAngularVelocity;
+        rb.maxAngularVelocity = Mathf.Infinity;
+        holder = t;
+
+    }
+
+    public void released(Transform t, Vector3 vel)
+    {
+        if (t == holder)
+        {
+            rb.useGravity = saveGravity;
+            rb.maxAngularVelocity = saveMaxAngularVelocity;
+            rb.velocity = vel;
+            holder = null;
+        }
+    }
+
+    public void snapped()
+    {
+        holder = null;
+        rb.useGravity = false;        
+        rb.isKinematic = true;
+    }
+}
---
+++ Vr_Robotics/Assets/snapIn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snapIn : MonoBehaviour
+{
+    public Transform local;
+    bool free = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)//other)
+    {
+        if (col.CompareTag("Block") && free)
+        {
+            Rigidbody other = col.gameObject.GetComponent<Rigidbody>();
+            other.GetComponent<PickupObject>().snapped();
+            Vector3 pos = new Vector3(local.position.x, local.position.y, local.position.z);
+            
+            other.transform.position = pos;//Vector3.MoveTowards(other.transform.position, pos, 2 * Time.deltaTime);
+            other.transform.parent = local.transform;
+
+            other.transform.localRotation = local.localRotation;
+            free = false;
+        }
+    }
+}
