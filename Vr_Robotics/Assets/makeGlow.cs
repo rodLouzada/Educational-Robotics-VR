7dbf8d8a6b132917c4575c9d85064965d29eae71
rodLouzada <rodrigolouzada1@gmail.com>
2019-04-14 20:00:57 -0400

    Glowing
    Edited the initial table, creating buttons to start the guided build;
    Created the secundary table top for the guided step by step;
    Code to make blocks to glow to indicate which object to interact with on stp by step guide.

---
+++ Vr_Robotics/Assets/makeGlow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeGlow : MonoBehaviour
+{
+    public GameObject glow;
+    public bool makeItGlow = false;
+
+    public void MakeGlow ()
+    {
+        Behaviour halo = (Behaviour)glow.GetComponent("Halo");
+        halo.enabled = true;
+    }
+
+    public void StopGlow()
+    {
+        Behaviour halo = (Behaviour)glow.GetComponent("Halo");
+        halo.enabled = false;
+    }
+
+   /* void Update()
+    {
+        if (makeItGlow)
+        {
+            MakeGlow();
+        }
+        else StopGlow(); 
+    } */
+}
