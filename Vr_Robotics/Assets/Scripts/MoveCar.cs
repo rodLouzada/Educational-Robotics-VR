0787bc2714e410bf6279b7723abc6c329f54f599
Richard Morris <rallanmorris@gmail.com>
2019-05-04 16:17:04 -0400

    Created a basic script for moving the car. Getting a weird vr error 'DllNotFoundException: libovravatar'

---
+++ Vr_Robotics/Assets/Scripts/MoveCar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour
+{
+    GameObject car;
+    Vector3 move;
+    Rigidbody carRB;
+    public float carSpeed;
+
+    private void OnEnable()
+    {
+        //this.MoveForward();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = this.gameObject;
+        carRB = this.GetComponent<Rigidbody>();
+        move = this.gameObject.transform.position;
+    }
+
+    void MoveForward()
+    {
+        carRB.velocity = transform.forward * carSpeed;
+    }
+
+    void MoveBackward()
+    {
+        carRB.velocity = -transform.forward * carSpeed;
+    }
+
+    void RotateLeft()
+    {
+        transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * carSpeed, Space.World);
+    }
+
+    void RotateRight()
+    {
+        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * carSpeed, Space.World);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.MoveForward();
+        }
+    }
+}
