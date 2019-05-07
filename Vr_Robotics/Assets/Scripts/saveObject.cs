04de20ac27c991076d94293b41ffb61b3f277e2f
Richard Morris <rallanmorris@gmail.com>
2019-05-06 22:53:23 -0400

    Saving Prefabs
    Co-Authored-By: rodlouzada <rodlouzada@users.noreply.github.com>

---
+++ Vr_Robotics/Assets/Scripts/saveObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+
+public class saveObject : MonoBehaviour
+{
+    string localPath;
+    public GameObject objToSave;
+    DateTime date;
+    void Start()
+    {
+        date = DateTime.Now;
+        localPath = "Assets/Collection" + date.ToString() + ".prefab";
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        CreateNew(objToSave, localPath);
+    }
+
+    static void CreateNew(GameObject obj, string localPath)
+    {
+        //Create a new Prefab at the path given
+        GameObject prefab = PrefabUtility.SaveAsPrefabAsset(obj, localPath);
+        //PrefabUtility.ReplacePrefab(obj, prefab, ReplacePrefabOptions.ConnectToPrefab);
+    }
+}
